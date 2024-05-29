bcdd11a79613cafe15d61fa5072f859fbe74019e
douglasscdoug <douglasscdoug@gmail.com>
2024-05-29 18:40:48 -0300

    Solução implementada

---
+++ Desafio1-Multiplos-de-3-e-5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//Calcular a soma de todos os múltiplos de 3 ou 5, menores que o parâmetro "numero".
+
+static int SomaMultiplosDe3Ou5(int numero)
+{
+    int resultado = 0;
+
+    for(int i = 1; i < numero; i++)
+    {
+        if(i % 3 == 0 || i % 5 == 0)
+        {
+            resultado += i;
+        }
+    }
+
+    return resultado;
+}
+
+Console.WriteLine($"Total {SomaMultiplosDe3Ou5(10)}"); // Deve retornar 23
+Console.WriteLine($"Total {SomaMultiplosDe3Ou5(49)}"); // Deve retornar 543
+Console.WriteLine($"Total {SomaMultiplosDe3Ou5(1000)}"); // Deve retornar 233168
+Console.WriteLine($"Total {SomaMultiplosDe3Ou5(8456)}"); // Deve retornar 16687353
+Console.WriteLine($"Total {SomaMultiplosDe3Ou5(19564)}"); // Deve retornar 89301183
